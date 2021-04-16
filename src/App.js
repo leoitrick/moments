f4ce2f7cc28d62a416ba28b1892b3e91009c1dcd
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-04-15 22:15:50 -0400

    added animations with framer motion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 import AboutUS from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
+//router
+import { Switch, Route, useLocation } from "react-router-dom";
+//animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUS />
+      <Nav />
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUS />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,19 @@
+import React from "react";
+//animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+
+const ContactUs = () => {
+  return (
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <h1>Contact Us</h1>
+    </motion.div>
+  );
+};
+
+export default ContactUs;
